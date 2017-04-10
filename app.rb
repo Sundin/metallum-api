85ae2b70f29d11c0220e228152c24edb9e1bf434
Gustav Sundin <gustav@kicksort.se>
2017-04-10 16:31:59 +0200

    cleanup

--- app.rb
+++ app.rb
@@ -7,8 +7,19 @@ require_relative 'lib/band'
 require_relative 'lib/parse'
 require_relative 'lib/url'
 
-get '/' do
-  'Hello World!'
+get '/band/:name/:id' do
+  name = params['name']
+  id = params['id']
+  url = "http://www.metal-archives.com/bands/" + name + "/" + id
+  Band.show_band_page(Parse.get_url(url)).to_json
+end
+
+get '/album/:band/:name/:id' do
+  band = params['band']
+  name = params['name']
+  id = params['id']
+  url = "http://www.metal-archives.com/albums/" + band + "/" + name + "/" + id
+  Album.show_album_page(Parse.get_url(url)).to_json
 end
 
 get '/search/band_name/:name' do
@@ -33,37 +44,6 @@ get '/search/band_name/:name' do
   end
 
   result_array.to_json
-end
-
-get '/band_by_name_and_id/:name/:id' do
-  name = params['name']
-  id = params['id']
-  url = "http://www.metal-archives.com/bands/" + name + "/" + id
-  Band.show_band_page(Parse.get_url(url)).to_json
-end
-
-get '/band/:name' do
-  html = Parse.get_json(Url.BAND(params['name']))
-  direct_link = Nokogiri::HTML(html["aaData"][0][0]).css('a')
-  direct_link.map{ |link|
-    res = Band.show_band_page(Parse.get_url(link['href']))
-    res.to_json
-  }
-end
-
-get '/album/:band/:name/:id' do
-  band = params['band']
-  name = params['name']
-  id = params['id']
-  url = "http://www.metal-archives.com/albums/" + band + "/" + name + "/" + id
-  Album.show_album_page(Parse.get_url(url)).to_json
-
-  # html = Parse.get_json(Url.ALBUM(params['name']))
-  # direct_link = Nokogiri::HTML(html["aaData"][0][1]).css('a')
-  # direct_link.map{ |link|
-  #   res = Album.show_album_page(Parse.get_url(link['href']))
-  #   res.to_json
-  # }
 end
 
 get '/search/album_name/:name' do
@@ -102,8 +82,4 @@ get '/search/album_name/:name' do
   end
 
   result_array.to_json
-end
-
-get '/:number' do
-  params['number']
 end
