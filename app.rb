f185ac392989d7b2ec5d50a969aa2484a60c6254
Gustav Sundin <sundin666@gmail.com>
2017-11-10 11:13:24 +0100

    Removed MongoDB, this API will now simply return JSON

--- app.rb
+++ app.rb
@@ -1,37 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
-require 'mongo'
 require_relative 'lib/crawler'
-require_relative 'lib/searcher'
-
-client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'test')
-db = client.database
-
 
 ### CRAWLING ###
-
-get '/crawl/:letter' do
-  # a - z, NBR, ~ 
-  letter = params['letter']
-  bands = Crawler.browse_bands(letter)
-  puts bands.count
-  bands.to_json
-end
 
 get '/quick_crawl/:letter' do
   letter = params['letter']
   bands = Crawler.quick_crawl(letter)
-  puts bands.count
   bands.to_json
-end
-
-get '/crawl/band/:name/:id' do
-  name = params['name']
-  id = params['id']
-
-  url = "https://www.metal-archives.com/bands/" + name + "/" + id
-  Crawler.crawl_band(url)
 end
 
 
@@ -58,19 +35,6 @@ end
 
 
 #### SEARCH ###
-
-get '/search/band_name/:band_name' do
-  band_name = params['band_name']
-  
-  search_results = Searcher.search_bands(band_name)
-
-  result = {
-    search_results: search_results,
-    query: band_name
-  }
-
-  result.to_json
-end
 
 get '/search/album_name/:title' do
   title = params['title']
